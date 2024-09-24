c994cca9cca8dcd3a0a1d3edc6de69625196a7e3
Kahu Arona-Haua (1000121223) <ARANK2@student.op.ac.nz>
2024-09-24 14:09:54 +1200

    updated code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
+        public static void kahu()
+        
+       {
+            Console.WriteLine("Hello my name is Kahu");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
